9b0b0bcea88ba518a18720388a8845a6f4e1ccee
whxie123 <whxie@uw.edu>
2024-11-06 00:05:37 -0800

    Update __init__.py

--- signalpathwaysimulator/__init__.py
+++ signalpathwaysimulator/__init__.py
@@ -1,15 +1,1 @@
-# signalpathwaysimulator/__init__.py
-# Initialize the SignalPathwaySimulator package
-
-version = '0.1.0'
-
-from .simulator import Simulator
-from .data_manager import DataManager
-from .visualization_manager import VisualizationManager
-
-all = [
-    'Simulator',
-    'DataManager',
-    'VisualizationManager'
-]
 
